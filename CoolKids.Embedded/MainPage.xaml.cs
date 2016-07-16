5cc79cb44d91549276174ee62e60bfe1664a64d9
Brian Kassay <Brian Kassay>
2016-07-15 22:24:33 -0700

    Added motion detector

--- CoolKids.Embedded/MainPage.xaml.cs
+++ CoolKids.Embedded/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using CoolKids.Uwp.Embedded.GPIO;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,30 @@ namespace CoolKids.Embedded
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Motion motionDetector;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            motionDetector = new Motion();
+            motionDetector.InitGPIO();
+            motionDetector.Changed += MotionDetector_Changed;
+
+            motionDetector.Start();
+        }
+
+        private void MotionDetector_Changed(bool MotionDetected)
+        {
+            if (MotionDetected)
+            {
+                tbMessage.Text = "Motion DETECTED";
+            }
+            else
+            {
+                tbMessage.Text = "Motion not detected";
+            }
+            //throw new NotImplementedException();
         }
     }
 }
